e361e20c8e307d62790fa9f443c31b5b37542111
vzooot <vzotikas@hotmail.com>
2023-09-21 17:55:28 +0200

    Initial Commit

---
+++ Znab/ZnabApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZnabApp.swift
+//  Znab
+//
+//  Created by Administrator on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ZnabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
